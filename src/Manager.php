f99c7dab16035fc7cdf7da5e96ada5e1fd9c64f9
Puleeno Nguyen <puleeno@gmail.com>
2020-02-22 13:57:17 +0700

    Move Manager class with condition class not exists

--- src/Manager.php
+++ src/Manager.php
@@ -6,37 +6,39 @@ use Jankx\PostLayout\Data\Post as PostData;
 use Jankx\PostLayout\Shortcodes\PostLayout;
 use Jankx\PostLayout\UI\RelatedPost;
 
-class Manager
-{
-    protected static $instance;
-
-    public static function instance()
+if (!class_exists(Manager::class)) {
+    class Manager
     {
-        if (is_null(self::$instance)) {
-            self::$instance = new self();
+        protected static $instance;
+
+        public static function instance()
+        {
+            if (is_null(self::$instance)) {
+                self::$instance = new self();
+            }
+
+            return self::$instance;
         }
 
-        return self::$instance;
-    }
-
-    public function __construct()
-    {
-        if (Jankx::isRequest('frontend')) {
-            add_action('jankx_setup_environment', array($this, 'setupEnvironment'));
+        public function __construct()
+        {
+            if (Jankx::isRequest('frontend')) {
+                add_action('jankx_setup_environment', array($this, 'setupEnvironment'));
+            }
+            add_action('init', array($this, 'registerPostLayoutShortcodes'));
         }
-        add_action('init', array($this, 'registerPostLayoutShortcodes'));
-    }
 
-    public function setupEnvironment($jankx)
-    {
-        $jankx->post = function ($postID = null) {
-            return new PostData($postID);
-        };
-    }
+        public function setupEnvironment($jankx)
+        {
+            $jankx->post = function ($postID = null) {
+                return new PostData($postID);
+            };
+        }
 
-    public function registerPostLayoutShortcodes()
-    {
-        $postLayout = new PostLayout();
-        add_shortcode('jankx_post_layout', array(new PostLayout(), 'render'));
+        public function registerPostLayoutShortcodes()
+        {
+            $postLayout = new PostLayout();
+            add_shortcode('jankx_post_layout', array(new PostLayout(), 'render'));
+        }
     }
 }
