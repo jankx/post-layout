f655436bcc8f542d45626c7ddd856b1effae907f
Puleeno Nguyen <puleeno@gmail.com>
2020-09-14 10:28:08 +0700

    Move render template structure to class

--- src/Layout/LargePostWithList.php
+++ src/Layout/LargePostWithList.php
@@ -13,10 +13,38 @@ class LargePostWithList extends PostLayout
                 'wp_query' => $this->wp_query
             )
         );
+        ?>
+        <div class="jankx-posts left-post right-list">
+            <?php
+                jankx_template('common/header-text', array(
+                    'text' => $args['header_text'],
+                ));
+            ?>
+            <div class="posts-list-wrapper">
+                    <div class="jankx-inner">
+                        <?php
+                        // Create first post
+                        $this->wp_query->the_post();
+                        $data = array(
+                            'post' => $this->wp_query->post,
+                        );
+                        jankx_template('post/loop/large-image', $data);
 
-        return jankx_template(
-            sprintf('layout/posts/%1$s-large-post-%2$s-list', 'left', 'right'),
-            $args
-        );
+
+                        // Create post list
+                        jankx_post_loop_start();
+
+                        while ($this->wp_query->have_posts()) {
+                            $this->wp_query->the_post();
+                            jankx_template('post/loop/list-item', $data);
+                        }
+
+                        jankx_post_loop_end();
+                        wp_reset_postdata();
+                        ?>
+                    </div>
+            </div>
+        </div>
+        <?php
     }
 }
