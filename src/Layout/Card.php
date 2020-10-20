1977d84c9c357705146374361a96c9abb0993a7e
Puleeno Nguyen <puleeno@gmail.com>
2020-10-21 02:00:58 +0700

    Create card layout

--- src/Layout/Card.php
+++ src/Layout/Card.php
@@ -3,7 +3,61 @@ namespace Jankx\PostLayout\Layout;
 
 use Jankx\PostLayout\PostLayout;
 
-class Card extends PostLayout {
-    public function render() {
+class Card extends PostLayout
+{
+    protected function open()
+    {
+    }
+    protected function close()
+    {
+    }
+
+    public function render()
+    {
+        $args = wp_parse_args(
+            $this->options,
+            array(
+                'wp_query' => $this->wp_query,
+                'large_first_post' => false,
+                'show_thumbnail' => true,
+                'thumbnail_position' => 'left',
+                'header_text' => '',
+            )
+        );
+        ?>
+        <div class="jankx-posts-layout list">
+            <?php
+            if ($args['header_text']) {
+                jankx_template('common/header-text', array(
+                    'text' => $args['header_text'],
+                    'url ' => $args['view_all_url'],
+                ));
+            }
+            ?>
+            <div class="posts-layout-wrapper">
+                <?php
+                // Create post list
+                jankx_post_loop_start('card', $args);
+
+                while ($this->wp_query->have_posts()) {
+                    $this->wp_query->the_post();
+                    $post = $this->wp_query->post;
+                    $data = array(
+                        'post' => $post,
+                        'show_thumbnail' => $args['show_thumbnail'],
+                        'post_class' => $this->getPostClass($post, $args),
+                    );
+                    jankx_template(array(
+                        $post->post_type . '/card/loop-item',
+                        'post/card/loop-item',
+                    ), $data);
+                }
+
+                jankx_post_loop_end('card', $args);
+                wp_reset_postdata();
+                ?>
+            </div>
+        </div>
+        <?php
     }
 }
