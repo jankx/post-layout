c9ee73126950eb59fc8cbbf97a683034f1ec7858
Puleeno Nguyen <puleeno@gmail.com>
2020-10-21 02:00:45 +0700

    update post layout funcs

--- functions.php
+++ functions.php
@@ -1,21 +1,28 @@
 <?php
-if (!function_exists('jankx_post_loop_start')) {
-    function jankx_post_loop_start($list_type = 'normal') {
-        $loop_start = jankx_template(
-            'post/loop-start',
-            compact('list_type'),
-            'start_loop_post',
-            false
-        );
-        echo apply_filters('jankx_post_loop_start', $loop_start, $list_type);
-    }
+use Jankx\PostLayout\PostLayoutManager;
+
+function jankx_post_loop_start($list_type = PostLayoutManager::LIST_LAYOUT, $args = null) {
+    $postsListClasses = array('jankx-posts');
+    $attributes = array(
+        'class' => $postsListClasses,
+    );
+
+    echo '<div ' . jankx_generate_html_attributes($attributes) . '>';
+
+    // This hook use to start custom render post layout
+    do_action('jankx_post_loop_start', $list_type, $args);
 }
 
-if (!function_exists('jankx_post_loop_end')) {
-    function jankx_post_loop_end() {
-        $loop_end = jankx_template('post/loop-end', array(), 'end_loop_post', false);
+function jankx_post_loop_end($list_type = null, $args = null) {
+    // This hook use to stop custom render post layout
+    do_action('jankx_post_loop_end', $list_type, $args);
 
-        echo apply_filters('jankx_post_loop_end', $loop_end);
-    }
+    // Close posts list wrapper
+    echo '</div><!-- End .jankx-posts -->';
+}
+
+function jankx_post_layout_wrapper($close = false) {
+    $action = $close ? 'close' : 'open';
+    do_action("jankx_post_layout_wrapper_{$action}");
 }
 
