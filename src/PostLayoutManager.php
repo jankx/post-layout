c3cc96555343258a01c4b3a9f69dedde23787a10
Puleeno Nguyen <puleeno@gmail.com>
2021-07-26 12:23:41 +0700

    Update preset 2 layout

--- src/PostLayoutManager.php
+++ src/PostLayoutManager.php
@@ -16,7 +16,7 @@ use Jankx\PostLayout\Layout\Preset2;
 
 class PostLayoutManager
 {
-    const VERSION = '1.0.0.42';
+    const VERSION = '1.0.0.48';
 
     protected static $instances;
     protected static $supportedLayouts;
